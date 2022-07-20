7b3427cc88aaa62afb4d9c74a5c9882d7051b430
Jen <6406037+usrrname@users.noreply.github.com>
2022-07-20 10:20:47 -0400

    doc(hooks/useFetch.tsx): add comment

--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -1,5 +1,5 @@
 import useSWR from "swr";
-
+// for client-side requests in next.js
 export const fetcher = (args: any) => fetch(args).then((res) => res.json());
 
 export const useUser = (apiUrl: string) => {
