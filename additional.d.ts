a3c11b18f220358e8656f790907fe66ddd7d5909
Jen <6406037+usrrname@users.noreply.github.com>
2022-07-20 13:40:14 -0400

    feat: adds type definition for post

--- additional.d.ts
+++ additional.d.ts
@@ -5,3 +5,16 @@ export type Page<P = {}> = NextPage<P> & {
   getLayout?: (page: ReactElement) => ReactNode;
   layout?: ComponentType;
 };
+
+export type PostType = {
+  slug: string;
+  title: string;
+  date: string;
+  coverImage: string;
+  author: string;
+  excerpt: string;
+  ogImage: {
+    url: string;
+  };
+  content: string;
+};
