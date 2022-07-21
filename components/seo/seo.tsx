e6dcbddc47be280fbd8eefa67268074491fd289c
Jen <6406037+usrrname@users.noreply.github.com>
2022-07-21 14:07:56 -0400

    refactor(seo.tsx): make seo component support more than config input but also posts

--- components/seo/seo.tsx
+++ components/seo/seo.tsx
@@ -1,22 +1,38 @@
 import Head from "next/head";
 import config from "../../config";
-
+import PostType from "../../typings.d";
 export default function SEO({ config }: any): JSX.Element {
-  const { title, description, siteTitle } = config as any;
+  if (config) {
+    const { title, description, siteTitle } = config as any;
+  }
+  if (typeof config === PostType) {
+    const { title, excerpt, content } = config as any;
+  }
 
   return (
     <Head>
-      <title>{`${title} | ${siteTitle}`}</title>
+      <title>{`${config?.title} | ${config?.siteTitle}`}</title>
       <meta name="viewport" content="width=device-width" />
-      <meta name="description" content={config.description} />
+      <meta
+        name="description"
+        content={config?.description || config?.excerpt}
+      />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:site_name" content={siteTitle} />
-      <meta property="twitter:card" content="summary" />
-      <meta property="twitter:creator" content={config.social.twitter} />
-      <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={description} />
+      <meta property="og:title" content={config?.title} />
+      <meta
+        property="og:description"
+        content={config?.description || config?.excerpt}
+      />
+      {config?.siteTitle && (
+        <meta property="og:site_name" content={config?.siteTitle} />
+      )}
+      <meta property="twitter:card" content={config?.excerpt} />
+      <meta property="twitter:creator" content={config?.social?.twitter} />
+      <meta property="twitter:title" content={config?.title} />
+      <meta
+        property="twitter:description"
+        content={config?.description || config?.excerpt}
+      />
     </Head>
   );
 }
