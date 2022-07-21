801f76d9bd9c6b214ad5ef00366dfe44ced26444
Jen <6406037+usrrname@users.noreply.github.com>
2022-07-21 07:53:45 -0400

    refactor(next.config.js): multiple plugins should export a single next config obj
    feat(typings.d.ts): declare remark and fs-extra modules
    fix(next.config.js): configure webpack to not import fs when not server environment
    
    lint(tsconfig.json): include custom type interfaces as part of compile
    
    setup: install fs-extra instead of fs

--- next.config.js
+++ next.config.js
@@ -1,26 +1,35 @@
+const withPlugins = require('next-compose-plugins');
 const { PHASE_PRODUCTION_BUILD, PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 /** @type {import('next').NextConfig} */
 const nextEnv = require('next-env')
 const dotenvLoad = require('dotenv-load')
 
-
 // loads env vars
 dotenvLoad()
-// creates plugin
-const withNextEnv = nextEnv()
+// creates plugins
 const withEnv = nextEnv()
-
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-    providerImportSource: "@mdx-js/react",
-  },
+const withMDX = require('@next/mdx')()
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 })
 
-const nextConfig = {
+const globalConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+        path: false
+      }
+    }
+    return config;
+  },
+  experimental: {
+    runtime: 'nodejs',
+    serverComponents: true,
+  },
   swcMinify: true,
+  output: 'standalone',
   poweredByHeader: false,
   reactStrictMode: true,
   pageExtensions: ['mdx', 'md', 'tsx', 'ts'],
@@ -30,18 +39,29 @@ const nextConfig = {
   images: {
     domains: ["randomuser.me", "mocky.io", "fonts.cdnfonts.com"],
     deviceSizes: [360, 375, 414, 640, 768, 828, 1080, 1200, 1920, 2048, 3840],
-    formats: ['image/webp'],
+    formats: ['image/jpg'],
   },
-  // productionBrowserSourceMaps: true,
-};
+  productionBrowserSourceMaps: true,
+}
 
-
-module.exports = (phase, { nextConfig }) => {
+module.exports = (phase, { globalConfig }) => {
   if (phase === PHASE_PRODUCTION_BUILD ? process.env.NODE_ENV === 'production' : null) {
     console.log('Production build')
   } if (phase === PHASE_DEVELOPMENT_SERVER) {
     console.log('Development mode')
   }
-  const mdxConfig = withMDX(nextConfig)
-  return withEnv(mdxConfig)
+  const nextConfig = withPlugins([
+    [withMDX, {
+      extension: /\.mdx?$/,
+      options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+        providerImportSource: "@mdx-js/react",
+      },
+    }],
+    [withBundleAnalyzer],
+    [withEnv],
+    globalConfig
+  ])
+  return { nextConfig }
 }
---
+++ typings.d.ts
@@ -0,0 +1,28 @@
+import { NextPage } from "next";
+import { ComponentType, ReactElement, ReactNode } from "react";
+
+export type Page<P = {}> = NextPage<P> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+  layout?: ComponentType;
+};
+
+export type PostType = {
+  slug: string;
+  title?: string;
+  date?: string;
+  coverImage?: string;
+  author?: string;
+  excerpt?: string;
+  ogImage?: {
+    url?: string;
+  };
+  content?: string;
+};
+
+export type PostProps = {
+  post: PostType;
+  allPosts: PostType[];
+};
+
+declare module "fs-extra";
+declare module "remark-html";
